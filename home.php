6751fa9da97c30c6b4c4669f7095f67165d8f12f
isisj <1078160@hr.nl>
2024-01-15 10:33:55 +0100

    Reconnected CSS

--- home.php
+++ home.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=, initial-scale=1.0">
     <title>homepage</title>
-    <link rel="stylesheet" href="csstyle.css">
+    <link rel="stylesheet" href="css/style.css">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
