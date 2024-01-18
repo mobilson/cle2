71a49ea3c0ea06abc14f769055f8f235c61ff032
stefanmuijs <smmuijs2002@gmail.com>
2024-01-18 14:25:41 +0100

    form validation login

---
+++ privacy.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<p>Praktijk voor mondhygiëne gebruikt patiëntengegevens alleen voor het doel waarvoor de gegevens zijn opgeslagen. Praktijk voor mondhygiëne deelt patiëntengegevens niet met derden, tenzij dit voor het opslagdoel nodig is. Praktijk voor mondhygiëne bewaart patiëntengegevens niet langer dan nodig is op basis van het opslagdoel van de gegevens. Praktijk voor mondhygiëne houdt met alle mogelijke middelen en maatregelen patiëntengegevens veilig voor inzage van onbevoegden. Praktijk voor mondhygiëne vraagt toestemming aan de patiënten voor het opslaan van persoonsgegevens, als er geen behandelcontract gesloten is. Praktijk voor mondhygiëne informeert patiënten over de rechten van de patiënt ten aanzien van zijn persoonsgegevens. Praktijk voor mondhygiëne informeert haar patiënten over het doel van de verwerking van persoonsgegevens. Praktijk voor mondhygiëne informeert patiënten indien Praktijk voor mondhygiëne bijzondere handelingen met de persoonsgegevens gaat verrichten. </p>
+</body>
+</html>
